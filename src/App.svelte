48252ec753f9ca020912bea9c42b482ee94a6dc3
andre347 <aedevries19@gmail.com>
2019-06-30 16:35:33 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  let newFruit = "";
+  let fruit = ["Apple", "Banana"];
+
+  function addFruit() {
+    console.log("Adding a new fruit...");
+    fruit = [newFruit, ...fruit];
+    newFruit = "";
+  }
+
+  function removeFruit(fruitToRemove) {
+    console.log("removing..");
+    console.log(fruitToRemove);
+    fruit = fruit.filter(f => f !== fruitToRemove);
+  }
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+
+  h4 {
+    font-style: italic;
+  }
+
+  li {
+    text-decoration: none;
+    margin: 10px;
+  }
+</style>
+
+<h1>List of Fruit</h1>
+<h4>Click fruit to remove..</h4>
+
+<input type="text" placeholder="Add Fruit..." bind:value={newFruit} />
+<button on:click={addFruit}>Add Fruit</button>
+
+<ul>
+  {#each fruit as f}
+    <li on:click={() => removeFruit(f)}> {f} </li>
+  {/each}
+</ul>
